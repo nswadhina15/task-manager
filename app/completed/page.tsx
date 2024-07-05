f3776148d1cb5cdfe86589741b3536690fcc73f0
Swadhina <swadhina.nayak1251@gmail.com>
2024-07-05 21:19:30 +0530

    create pages

---
+++ app/completed/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>completed</div>
+  )
+}
+
+export default page
---
+++ app/important/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>important</div>
+  )
+}
+
+export default page
